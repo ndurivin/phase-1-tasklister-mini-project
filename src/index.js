d6be96b8e3b3717de1891efac96d480a2c3025dc
Vinn Nduri <ndurivo@gmail.com>
2022-06-14 18:02:09 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskToDo(document.getElementById('new-task-description').value);
+    form.reset()
+  });
 });
+
+function taskToDo(todo) {
+  let li = document.createElement("li");
+  li.textContent = `${todo} `;
+  let liBtn = document.createElement("button");
+  liBtn.addEventListener('click', handleDelete)
+  liBtn.textContent = "x";
+  li.appendChild(liBtn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
